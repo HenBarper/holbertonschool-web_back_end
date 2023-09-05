ce45baaebfefca73270845776fd61366c82534fa
Ben Harper <benjaminitonio@gmail.com>
2023-09-05 14:36:58 -0500

    5 v2

--- ES6_basic/5-spread-operator.js
+++ ES6_basic/5-spread-operator.js
@@ -1,14 +1,7 @@
 export default function concatArrays(array1, array2, string) {
-  const array = [];
+  let array = [];
 
-  for (const character of array1) {
-    array.push(character);
-  }
-  for (const character of array2) {
-    array.push(character);
-  }
-  for (const character of string) {
-    array.push(character);
-  }
+  array = [...array1, ...array2, ...string];
+
   return (array);
 }
