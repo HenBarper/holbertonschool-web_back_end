bf12aa46ca48d763e0f807b85229f1fff14e3943
henbarper <benjaminitonio@gmail.com>
2023-09-12 14:19:05 -0500

    8 v4

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -6,8 +6,10 @@ export default function cleanSet(set, startString) {
   const newStringSet = new Set();
 
   for (const str of set) {
-    if (str.includes(startString)) {
-      newStringSet.add(str.replace(startString, ''));
+    if (str) {
+      if (str.includes(startString)) {
+        newStringSet.add(str.replace(startString, ''));
+      }
     }
   }
 
