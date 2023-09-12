13ebc9e0b55948d6646fd6ff49b43f8ea6df3a1b
henbarper <benjaminitonio@gmail.com>
2023-09-12 13:22:37 -0500

    5 v2

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -6,5 +6,5 @@ export default function createInt8TypedArray(length, position, value) {
   const arrayBuffer = new ArrayBuffer(length);
   const view = new DataView(arrayBuffer);
   view.setInt8(position, value);
-  return arrayBuffer;
+  return view;
 }
